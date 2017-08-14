1d5e78059e53b22a2ce1d842e168c8137af1a170
Shubham-invent <shubham.invent@gmail.com>
2017-08-15 00:37:02 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('shubham442/docker-example')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('shubham442')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
